21e3911a708cefeda53b8bc79ce723f334aa21bc
Duchvph17480 <duchvph17480@fpt.edu.vn>
2022-05-18 17:02:28 +0700

    aa

--- src/main/java/com/IT16308/ResourceConfig.java
+++ src/main/java/com/IT16308/ResourceConfig.java
@@ -13,7 +13,7 @@ public class ResourceConfig {
 		ReloadableResourceBundleMessageSource ms =
 			new ReloadableResourceBundleMessageSource();
 		
-		ms.setBasenames("classpath:messages/User");
+		ms.setBasenames("classpath:messages/user");
 		ms.setDefaultEncoding("UTF-8");
 		
 		return ms;
